03ba7f61f3938e0e03fba0f8876df407e74c862d
iamjhpark <91qkrwjdgns@naver.com>
2021-10-04 23:06:31 +0900

    commit

---
+++ class-note/1_type-basic.js
@@ -0,0 +1,5 @@
+var str = 'hello';
+var num = 10;
+var arr = [];
+var heroes = ['Capt', 'Thor', 'Hulk'];
+var items;
---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,5 @@
+const str: string = 'hello';
+const num: number = 10;
+const arr: Array<number> = [];
+const heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+const items: number[] = [1, 2, 3];
